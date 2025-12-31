4caf2aca86cdc8188ef3815b0d632ae1490fb477
HoeumSenghun <hoeumsenghun369@gmail.com>
2025-12-31 10:14:17 +0700

    Happy New Year

---
+++ src/test/java/com/example/springyearend2025/SpringYearEnd2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springyearend2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringYearEnd2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
